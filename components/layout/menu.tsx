0bca5474a313ed44e245b7ea6b3dcf69b7ac5788
bokyum <95bogyeom@gmail.com>
2024-11-27 15:14:08 +0900

    layout 오류 수정

--- components/layout/menu.tsx
+++ components/layout/menu.tsx
@@ -7,7 +7,7 @@ interface MenuProps {
 export default function Menu({ links }: MenuProps) {
   const renderMenu = (menuItems: MenuItem[]) => {
     return (
-      <ul className="menu ">
+      <ul className="menu menu-xs ">
         {menuItems.map((item, index) => (
           <li key={index}>
             {item.children && item.children.length > 0 ? (
@@ -29,17 +29,19 @@ export default function Menu({ links }: MenuProps) {
       </ul>
     );
   };
-
   return (
-    <div className="drawer-side top-16 lg:top-0 h-[calc(100svh-4rem)] overflow-y-auto  bg-base-200">
+    <div className="drawer-side mt-16 lg:mt-0">
       <label
         htmlFor="drawer"
         className="drawer-overlay"
         aria-label="Close menu"
       ></label>
-      <aside className="h-full w-80 text-base-content space-y-4 bg-base-200 z-1">
-        <ul className="menu px-4 py-0">{renderMenu(links)}</ul>
-      </aside>
+
+      <div className="h-full  overflow-y-auto bg-base-200">
+        <aside className="w-80 bg-base-200 text-base-content ">
+          {renderMenu(links)}
+        </aside>
+      </div>
     </div>
   );
 
